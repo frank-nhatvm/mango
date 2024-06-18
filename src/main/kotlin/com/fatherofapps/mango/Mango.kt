bfa9abb7e37d1ba7868548f7bfe2d7f644d6861b
nhatvu <nhat.thtb@gmail.com>
2024-06-18 16:07:47 +0700

    add demo encode

--- src/main/kotlin/com/fatherofapps/mango/Mango.kt
+++ src/main/kotlin/com/fatherofapps/mango/Mango.kt
@@ -1,4 +1,13 @@
 package com.fatherofapps.mango
 
+import org.apache.commons.codec.binary.Base64
+
 object Mango {
+    fun encode(message: String) : String{
+        return String(Base64.encodeBase64(message.toByteArray()))
+    }
+
+    fun decode(message: String): String{
+        return String(Base64.decodeBase64(message))
+    }
 }
